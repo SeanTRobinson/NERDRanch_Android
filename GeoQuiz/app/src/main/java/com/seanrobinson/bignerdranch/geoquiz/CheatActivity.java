ccdcd2dd20a2a2861ff35afc9ec65ce8db96abcc
Sean Robinson <seanrobinson@Seans-MacBook-Pro.local>
2014-09-20 14:09:10 +0100

    Added other questions, rotation and previous button

---
+++ GeoQuiz/app/src/main/java/com/seanrobinson/bignerdranch/geoquiz/CheatActivity.java
@@ -0,0 +1,15 @@
+package com.seanrobinson.bignerdranch.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by seanrobinson on 19/09/2014.
+ */
+public class CheatActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
---
+++ GeoQuiz/app/src/main/java/com/seanrobinson/bignerdranch/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.seanrobinson.bignerdranch.geoquiz;
+
+/**
+ * Created by seanrobinson on 19/09/2014.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
