59743222f5f87da43c56d18987a4077051f9e015
Sean Robinson <Sean.T.Robinson@googlemail.com>
2014-09-21 18:57:29 +0100

    Finished pager.

--- CriminalIntent/app/src/main/java/com/seanrobinson/bnr/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/seanrobinson/bnr/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.seanrobinson.bnr.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,22 +10,45 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
-    public UUID getmId() {
+    public UUID getId() {
         return mId;
     }
 
-    public String getmTitle() {
+    public String getTitle() {
 
         return mTitle;
     }
 
-    public void setmTitle(String mTitle) {
+    public void setTitle(String mTitle) {
         this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
     }
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
 
+    @Override
+    public String toString() {
+        return mTitle;
     }
 }
--- CriminalIntent/app/src/main/java/com/seanrobinson/bnr/criminalintent/CriminalActivity.java
+++ CriminalIntent/app/src/main/java/com/seanrobinson/bnr/criminalintent/CriminalActivity.java
@@ -1,22 +1,14 @@
 package com.seanrobinson.bnr.criminalintent;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.UUID;
 
+import android.support.v4.app.Fragment;
 
-public class CriminalActivity extends Activity {
-
+public class CriminalActivity extends SingleFragmentActivity {
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected Fragment createFragment() {
+        UUID crimeId = (UUID)getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
